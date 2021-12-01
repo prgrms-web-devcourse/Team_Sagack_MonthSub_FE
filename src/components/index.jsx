8269fd68145db94e582e5cdde7beb625be88d561
thddlmy <thddlmy@gmail.com>
2021-12-01 17:24:52 +0900

    chore: issue 템플릿 삭제, stories 폴더 정리, js 확장자 변경, empty 파일 삭제

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,8 +2,4 @@ export { default as List } from './commons/List';
 export { default as Modal } from './commons/Modal';
 export { default as Button } from './commons/Button';
 export { default as Header } from './domain/Header';
-export { default as LoginForm } from './domain/LoginForm'
-
-
-
-
+export { default as LoginForm } from './domain/LoginForm';
