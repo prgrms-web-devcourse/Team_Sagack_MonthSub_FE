185a50545e3a589a8b4eaab9b7c3758cb14d85de
yoon <yunyun40550082@gmail.com>
2021-12-01 15:07:05 +0900

    feat: modal컴포넌트 구현

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as List } from './commons/List';
+export { default as Modal } from './commons/Modal';
 export { default as Header } from './domain/Header';
-
 export { default as LoginForm } from './domain/LoginForm'
 
 
