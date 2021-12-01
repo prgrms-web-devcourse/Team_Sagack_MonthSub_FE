9bce84eb51297e2c6b4a1331113345a5198e1c33
thddlmy <thddlmy@gmail.com>
2021-12-01 17:50:52 +0900

    feat: wrapper 컴포넌트 구현

---
+++ src/components/commons/Wrapper/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const Wrapper = ({ children, width }) => (
+  <StyledDiv width={width}>{children}</StyledDiv>
+);
+
+export default Wrapper;
+
+Wrapper.defaultProps = {
+  width: '71.25rem',
+};
+
+Wrapper.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
+
+const StyledDiv = styled.div`
+  text-align: center;
+  border: 2px #000000 solid;
+  width: ${props => props.width || '71.25rem'};
+`;
