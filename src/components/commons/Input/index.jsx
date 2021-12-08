c6c9d584075234952cbea98dfdc5887b2e09ba90
thddlmy <thddlmy@gmail.com>
2021-12-08 16:07:37 +0900

    feat: 글 작성 리스트 페이지 마크업, 더미데이터, 링크 작업

--- src/components/commons/Input/index.jsx
+++ src/components/commons/Input/index.jsx
@@ -41,7 +41,6 @@ Input.defaultProps = {
   title: '',
   value: '',
   onChange: () => {},
-  value: '',
   name: '',
 };
 
--- src/components/domain/CardList/index.jsx
+++ src/components/domain/CardList/index.jsx
@@ -3,75 +3,63 @@ import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import convertCategory from '@utils/convertCategory';
-import { Icons, Image } from '@components'
+import { Icons, Image } from '@components';
 
 const CardList = ({ list, ...props }) => (
-    <CardContainer {...props}>
-      {
-        list.map(( item ) =>
-          <Card key={ item.series.id }>
-            {
-              item.subscribe.status === 'SUBSCRIPTION_AVAILABLE' ?
-                <SubscribeStatusDiv>
-                  <div className='available'>모집중</div>
-                </SubscribeStatusDiv> :
-                <SubscribeStatusDiv>
-                <div className='unAvailable'>연재중</div>
-              </SubscribeStatusDiv>
-            }
-            <div className='card-imageArea'>
-              <Link to={`/series/${ item.series.id }`}>
-                <Image src={ item.series.thumbnail } width='100%' height='100%' />
+  <CardContainer {...props}>
+    {list.map(item => (
+      <Card key={item.series.id}>
+        {item.subscribe.status === 'SUBSCRIPTION_AVAILABLE' ? (
+          <SubscribeStatusDiv>
+            <div className="available">모집중</div>
+          </SubscribeStatusDiv>
+        ) : (
+          <SubscribeStatusDiv>
+            <div className="unAvailable">연재중</div>
+          </SubscribeStatusDiv>
+        )}
+        <div className="card-imageArea">
+          <Link to={`/series/${item.series.id}`}>
+            <Image src={item.series.thumbnail} width="100%" height="100%" />
+          </Link>
+        </div>
+        <div className="card-textArea">
+          <div>
+            <div className="card-userId">
+              <Link to={`/channel/${item.writer.userId}`}>
+                {item.writer.nickname}
               </Link>
             </div>
-            <div className='card-textArea'>
-              <div>
-                <div className='card-userId'>
-                  <Link to={`/channel/${ item.writer.userId }`}>
-                    {item.writer.nickname}
-                  </Link>
-                </div>
-                <div className='card-likes'>
-                  <Icons.Like fontSize='1rem' />
-                  { item.series.likes } Likes
-                </div>
-              </div>
-              <div className="card-title">
-                <Link to={`/series/${item.series.id}`}>
-                  { item.series.title }
-                </Link>
-              </div>
-              <div>
-                { item.series.introduceSentence }
-              </div>
-              <div>
-                <div className='category'>
-                  {
-                    convertCategory(item.category)
-                  }
-                </div>
-                <div>
-                  {
-                    item.subscribe.status === 'SUBSCRIPTION_AVAILABLE' ?
-                      `모집마감 ~ ${ item.subscribe.endDate }` :
-                      `연재종료 ~ ${ item.series.endDate }`
-                  }
-                </div>
-              </div>
+            <div className="card-likes">
+              <Icons.Like fontSize="1rem" />
+              {item.series.likes} Likes
             </div>
-          </Card>
-        )
-      }
-    </CardContainer>
-  );
+          </div>
+          <div className="card-title">
+            <Link to={`/series/${item.series.id}`}>{item.series.title}</Link>
+          </div>
+          <div>{item.series.introduceSentence}</div>
+          <div>
+            <div className="category">{convertCategory(item.category)}</div>
+            <div>
+              {item.subscribe.status === 'SUBSCRIPTION_AVAILABLE'
+                ? `모집마감 ~ ${item.subscribe.endDate}`
+                : `연재종료 ~ ${item.series.endDate}`}
+            </div>
+          </div>
+        </div>
+      </Card>
+    ))}
+  </CardContainer>
+);
 
 CardList.defaultProps = {
   list: [],
-}
+};
 
 CardList.propTypes = {
   list: PropTypes.array,
-}
+};
 
 export default CardList;
 
@@ -79,7 +67,7 @@ const CardContainer = styled.div`
   width: 100%;
   height: auto;
   display: flex;
-  flex-flow : row wrap;
+  flex-flow: row wrap;
 `;
 
 /* 
@@ -94,17 +82,19 @@ const CardContainer = styled.div`
 const margin = '1.875rem';
 const contentsMaxCount = 4;
 const Card = styled.div`
-  width: calc((100% - (${ margin } * ${ contentsMaxCount - 1 })) / ${ contentsMaxCount });
-  margin-right: ${ margin };
+  width: calc(
+    (100% - (${margin} * ${contentsMaxCount - 1})) / ${contentsMaxCount}
+  );
+  margin-right: ${margin};
   margin-bottom: 2.5rem;
   display: flex;
   flex-direction: column;
   position: relative;
 
-  &:nth-of-type(${ contentsMaxCount }n) {
+  &:nth-of-type(${contentsMaxCount}n) {
     margin-right: 0;
   }
-  
+
   .card {
     &-imageArea {
       height: 11rem;
@@ -133,14 +123,14 @@ const Card = styled.div`
         align-items: center;
         border: 0.0625rem solid #ffb15c;
       }
-      
+
       > div {
         margin-bottom: 0.875rem;
         display: flex;
         justify-content: space-between;
         align-items: center;
       }
-      
+
       > div:last-child {
         margin-bottom: 0;
       }
@@ -150,13 +140,13 @@ const Card = styled.div`
           text-decoration: underline;
         }
       }
-      
+
       .card-title {
         font-size: 1.125rem;
         color: #000000;
         line-height: 1.5rem;
         flex-grow: 1;
-  
+
         &:hover {
           text-decoration: underline;
         }
@@ -169,7 +159,7 @@ const SubscribeStatusDiv = styled.div`
   position: absolute;
   top: 0;
   right: -0.625rem;
-  
+
   > * {
     padding: 0.625rem;
   }
