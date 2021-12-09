b908f3e5c49f0e3811fe884e46acf2ce103087e5
kokoke414 <kokoke414@gmail.com>
2021-12-09 17:40:33 +0900

    feat: css공통변수 정리(중간커밋)

---
+++ src/styles/theme.jsx
@@ -0,0 +1,29 @@
+const colors = {
+  mainLight: '#ffefde',
+  main: '#ffb15c',
+  sub: '#041b2d',
+  greyLight: '#f2f2f2',
+  grey: '#eaeaea',
+  greyMedium: '#bdbdbd',
+  greyDark: '#4b4b4b',
+  red: '#e00000',
+};
+
+const font = {
+  small: '0.875rem',
+  base: '1rem',
+  medium: '1.125rem',
+  large: '1.5rem',
+};
+
+const style = {
+  boxShadow: '1px 3px 5px 1px rgba(0, 0, 0, 0.3)',
+};
+
+const theme = {
+  colors,
+  font,
+  style,
+};
+
+export default theme;
