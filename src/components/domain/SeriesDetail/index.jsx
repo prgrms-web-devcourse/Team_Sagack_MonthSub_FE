90b9454ec0d56fd85bbbfa8598f609bcf60da493
yoon <yunyun40550082@gmail.com>
2021-12-14 12:40:28 +0900

    chore: 구현 안된 기능 주석 처리

--- src/components/domain/SeriesDetail/index.jsx
+++ src/components/domain/SeriesDetail/index.jsx
@@ -6,132 +6,128 @@ import {
   PageSectionContainer,
   PageSectionTitle,
   ArticleList,
-  CommentList,
+  // CommentList,
   Image,
 } from '@components';
 import { Link } from 'react-router-dom';
 
-const SeriesDetail = ({ detail }) => {
-  const commentList = [
-    {
-      commentId: 1,
-      nickname: '홍길동',
-      thumbnail: 'img',
-      text: '이야 이 서비스 끝내줍니다! 칭찬해~',
-      date: '2021-12-02',
-    },
-    {
-      commentId: 2,
-      nickname: '뽀식이',
-      thumbnail: 'img',
-      text: '마크업 힘들어.. 그만할래...ㅜ',
-      date: '2021-12-24',
-    },
-    {
-      commentId: 3,
-      nickname: '초롱이',
-      thumbnail: 'img',
-      text: '집갈래~',
-      date: '2021-12-31',
-    },
-  ];
+const SeriesDetail = ({ detail }) => (
+  // const commentList = [
+  //   {
+  //     commentId: 1,
+  //     nickname: '홍길동',
+  //     thumbnail: 'img',
+  //     text: '이야 이 서비스 끝내줍니다! 칭찬해~',
+  //     date: '2021-12-02',
+  //   },
+  //   {
+  //     commentId: 2,
+  //     nickname: '뽀식이',
+  //     thumbnail: 'img',
+  //     text: '마크업 힘들어.. 그만할래...ㅜ',
+  //     date: '2021-12-24',
+  //   },
+  //   {
+  //     commentId: 3,
+  //     nickname: '초롱이',
+  //     thumbnail: 'img',
+  //     text: '집갈래~',
+  //     date: '2021-12-31',
+  //   },
+  // ];
 
-  return (
-    <>
-      <ViewContainer>
-        <div className="imageWrapper">
-          <Image
-            src={detail.series.thumbnail}
-            width="100%"
-            height="100%"
-            alt="series-detail-image"
-          />
+  <>
+    <ViewContainer>
+      <div className="imageWrapper">
+        <Image
+          src={detail.series.thumbnail}
+          width="100%"
+          height="100%"
+          alt="series-detail-image"
+        />
+      </div>
+      <div className="viewArticle">
+        <div>
+          <div className="viewArticle-title">{detail.series.title}</div>
+          <div className="viewArticle-info">
+            <div className="userInfo">
+              <Link to={`/channel/${detail.writer.userId}`}>
+                <span>
+                  <Image
+                    src={detail.writer.profileImage}
+                    width="100%"
+                    height="100%"
+                    alt="user-profile"
+                  />
+                </span>
+                <span>{detail.writer.nickname}</span>
+              </Link>
+            </div>
+            <div>
+              <span>{detail.series.likes}</span> Likes
+            </div>
+          </div>
+          <div className="viewArticle-text">{detail.series.introduceText}</div>
+          <div className="viewArticle-edit">
+            <Link to={`/series/edit/${detail.series.id}`}>
+              <Button width="100%" height="3.125rem" font-size="1.5rem">
+                수정하기
+              </Button>
+            </Link>
+          </div>
         </div>
-        <div className="viewArticle">
-          <div>
-            <div className="viewArticle-title">{detail.series.title}</div>
-            <div className="viewArticle-info">
-              <div className="userInfo">
-                <Link to={`/channel/${detail.writer.userId}`}>
-                  <span>
-                    <Image
-                      src={detail.writer.profileImage}
-                      width="100%"
-                      height="100%"
-                      alt="user-profile"
-                    />
-                  </span>
-                  <span>{detail.writer.nickname}</span>
-                </Link>
-              </div>
+        <div>
+          <div className="viewArticle-priceInfo">
+            <div className="viewArticle-priceInfo-block">
+              <div>구독 모집일</div>
               <div>
-                <span>{detail.series.likes}</span> Likes
+                {detail.subscribe.startDate} ~ {detail.subscribe.endDate}
               </div>
             </div>
-            <div className="viewArticle-text">
-              {detail.series.introduceText}
-            </div>
-            <div className="viewArticle-edit">
-              <Link to={`/series/edit/${detail.series.id}`}>
-                <Button width="100%" height="3.125rem" font-size="1.5rem">
-                  수정하기
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div>
-            <div className="viewArticle-priceInfo">
-              <div className="viewArticle-priceInfo-block">
-                <div>구독 모집일</div>
+            <div className="viewArticle-priceInfo-block">
+              <div>연재 정보</div>
+              <div>
                 <div>
-                  {detail.subscribe.startDate} ~ {detail.subscribe.endDate}
+                  <div>총 회차</div>
+                  <div>{detail.series.articleCount}</div>
                 </div>
-              </div>
-              <div className="viewArticle-priceInfo-block">
-                <div>연재 정보</div>
                 <div>
+                  <div>연재 기간</div>
                   <div>
-                    <div>총 회차</div>
-                    <div>{detail.series.articleCount}</div>
+                    {detail.series.startDate} ~ {detail.series.endDate}
                   </div>
+                </div>
+                <div>
+                  <div>연재 주기</div>
                   <div>
-                    <div>연재 기간</div>
-                    <div>
-                      {detail.series.startDate} ~ {detail.series.endDate}
-                    </div>
-                  </div>
-                  <div>
-                    <div>연재 주기</div>
-                    <div>
-                      {detail.upload.date.map(day => day)},{detail.upload.time}
-                    </div>
+                    {detail.upload.date.map(day => day)},{detail.upload.time}
                   </div>
                 </div>
               </div>
-              <div className="viewArticle-priceInfo-block">
-                <div>구독료</div>
-                <div>{detail.series.price} 원</div>
-              </div>
-              <Link to="/purchase">
-                <Button width="100%" height="3.125rem" font-size="1.5rem">
-                  결제하기
-                </Button>
-              </Link>
             </div>
+            <div className="viewArticle-priceInfo-block">
+              <div>구독료</div>
+              <div>{detail.series.price} 원</div>
+            </div>
+            <Link to="/purchase">
+              <Button width="100%" height="3.125rem" font-size="1.5rem">
+                결제하기
+              </Button>
+            </Link>
           </div>
         </div>
-      </ViewContainer>
-      <PageSectionContainer>
-        <PageSectionTitle text="연재 목록" />
-        <ArticleList list={detail.articleList} />
-      </PageSectionContainer>
-      <PageSectionContainer>
-        <PageSectionTitle text="댓글 목록" />
-        <CommentList list={commentList} />
-      </PageSectionContainer>
-    </>
-  );
-};
+      </div>
+    </ViewContainer>
+    <PageSectionContainer>
+      <PageSectionTitle text="연재 목록" />
+      <ArticleList list={detail.articleList} />
+    </PageSectionContainer>
+    <PageSectionContainer>
+      <PageSectionTitle text="댓글 목록" />
+      {/* <CommentList list={commentList} /> */}
+    </PageSectionContainer>
+  </>
+);
 SeriesDetail.defaultProps = {
   detail: {},
 };
