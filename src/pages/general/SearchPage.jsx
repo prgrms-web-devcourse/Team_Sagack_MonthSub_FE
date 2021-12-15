ed496f0ef2f01e45c8df8de62dc877ad19df2f2e
thddlmy <thddlmy@gmail.com>
2021-12-15 15:26:25 +0900

    feat: [MON-165] 검색 페이지 마크업 일부

--- src/pages/general/SearchPage.jsx
+++ src/pages/general/SearchPage.jsx
@@ -1,5 +1,54 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { Container, Icons, Input, IconWrapper } from '@components';
+import { useForm } from '@hooks';
 
-const SearchPage = () => <div />;
+const SearchPage = () => {
+  const { values, handleChange, handleSubmit } = useForm({
+    initialValues: {
+      keyword: '',
+    },
+    onSubmit: () => {},
+    validate: () => {},
+  });
+
+  return (
+    <Container>
+      <H1>검색</H1>
+      <SearchForm onSubmit={handleSubmit}>
+        <StyledIconWrapper>
+          <Icons.Search />
+        </StyledIconWrapper>
+        <StyledInput
+          width="100%"
+          name="keyword"
+          value={values.keyword}
+          onChange={handleChange}
+          placeholder="검색어를 입력하세요."
+        />
+      </SearchForm>
+    </Container>
+  );
+};
 
 export default SearchPage;
+
+const H1 = styled.h1`
+  width: 100%;
+  font-weight: 700;
+  font-size: 1.5rem;
+  padding: 0.5rem 0;
+`;
+
+const StyledInput = styled(Input)`
+  border-bottom: #000000 0.063rem solid;
+`;
+
+const SearchForm = styled.div`
+  position: relative;
+`;
+
+const StyledIconWrapper = styled(IconWrapper)`
+  position: absolute;
+  right: 0;
+`;
