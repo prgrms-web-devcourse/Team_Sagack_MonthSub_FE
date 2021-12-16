85b7bb70a2d29f47473921bbcdaf089640e8804c
yoon <yunyun40550082@gmail.com>
2021-12-16 15:46:17 +0900

    refactor: [MON-185] likeToggle 로직 수정

--- src/components/commons/LikeToggle/index.jsx
+++ src/components/commons/LikeToggle/index.jsx
@@ -3,34 +3,25 @@ import PropTypes from 'prop-types';
 import { Icons, IconWrapper } from '@components';
 import { useToggle } from '@hooks';
 
-const LikeSeriesIdList = [2, 3, 4, 5, 6, 7];
-
-export const LikeToggle = ({ seriesId }) => {
+export const LikeToggle = ({ seriesId, isLiked }) => {
   const [state, toggle] = useToggle();
 
   useEffect(() => {
-    // 전역 상태 가져오기
-    LikeSeriesIdList.includes(seriesId) && toggle();
+    isLiked && toggle();
   }, []);
 
-  const addLike = () => {
-    console.log('좋아요');
-    // 전역상태에서 seriesId추가
-    // 좋아요 추가 api호출
+  const addLike = async () => {
+    console.log('좋아요 추가 api호출', seriesId);
   };
 
-  const cancleLike = () => {
-    console.log('좋아요 취소');
-    // 전역상태에서 seriesId제거
-    // 좋아요 취소 api호출
+  const cancleLike = async () => {
+    console.log('좋아요 취소 api호출', seriesId);
   };
 
   const handleClick = () => {
-    state ? cancleLike() : addLike();
     toggle();
+    state ? cancleLike() : addLike();
   };
-
-  console.log(state);
 
   return (
     <div onClick={handleClick}>
@@ -43,9 +34,11 @@ export const LikeToggle = ({ seriesId }) => {
 
 LikeToggle.defaultProps = {
   seriesId: 2,
+  isLiked: false,
 };
 LikeToggle.propTypes = {
   seriesId: PropTypes.number,
+  isLiked: PropTypes.bool,
 };
 
 export default LikeToggle;
