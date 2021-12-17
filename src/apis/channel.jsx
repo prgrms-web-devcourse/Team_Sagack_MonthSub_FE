fc7bb7091f2c8adcc2fb3ef651eabb91d2fb9568
kokoke414 <kokoke414@gmail.com>
2021-12-17 19:27:27 +0900

    refactor: axios 업데이트에 맞게 수정

--- src/apis/channel.jsx
+++ src/apis/channel.jsx
@@ -3,11 +3,9 @@ import { GET } from './axios';
 export const getMyChannel = async () =>
   GET({
     url: `/channel/me`,
-    isAuth: true,
   });
 
 export const getChannel = async id =>
   GET({
     url: `/channel/users/${id}/other`,
-    isAuth: true,
   });
--- src/apis/series.jsx
+++ src/apis/series.jsx
@@ -1,35 +1,14 @@
-import axios from 'axios';
 import { GET, POST, PUT } from './axios';
 
-const { REACT_APP_API_END_POINT } = process.env;
-
-export const getSeriesScrolling = async ({
-  lastSeriesId,
-  size,
-  categories,
-}) => {
-  const response = await axios({
-    method: 'get',
-    url: `${REACT_APP_API_END_POINT}/series`,
-    params: {
-      lastSeriesId,
-      size,
-      categories,
-    },
-  });
-  return response;
-};
-
-export const getSeries = async () =>
+export const getSeries = async params =>
   GET({
-    url: '/series?size=16&categories=ALL',
-    isAuth: false,
+    url: '/series',
+    params,
   });
 
 export const getSeriesDetail = async ({ id }) =>
   GET({
     url: `/series/${id}`,
-    isAuth: false,
   });
 
 export const postSeries = async ({ data }) =>
