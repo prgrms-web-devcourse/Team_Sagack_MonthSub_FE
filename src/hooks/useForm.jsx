4ffe9b7e4ec7956f3e58dbf35e7cce59257e1fa9
yoon <yunyun40550082@gmail.com>
2021-12-21 15:36:02 +0900

    chore: 콘솔 제거

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -16,7 +16,6 @@ const useForm = ({ initialValues, onSubmit, validate }) => {
   const handleChange = e => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
-    console.log(value);
   };
 
   const handleSubmit = async e => {
