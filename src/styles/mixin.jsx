2994755bb9863f196cf3d4ce890024cd5ec5ccd8
kokoke414 <kokoke414@gmail.com>
2022-01-15 19:59:08 +0900

    style: 스크롤 바 숨김 mixin 추가

--- src/styles/mixin.jsx
+++ src/styles/mixin.jsx
@@ -7,6 +7,14 @@ const fullScreen = css`
   margin-left: calc(-50vw + 50%);
 `;
 
-const mixin = { fullScreen };
+const invisibleScrollBar = css`
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+`;
+
+const mixin = { fullScreen, invisibleScrollBar };
 
 export default mixin;
