9ee5fcff45e4d5baa6563cfa18639919a884be72
kokoke414 <kokoke414@gmail.com>
2022-01-15 19:59:29 +0900

    style: theme에 변수들 적용

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -26,22 +26,61 @@ const common = {
 };
 
 const deviceSizes = {
-  mobile: '375px',
-  tablet: '768px',
-  laptop: '1024px',
+  mobile: 480,
+  tablet: 768,
+  laptop: 1140,
 };
 
 const device = {
-  mobile: `screen and (max-width: ${deviceSizes.tablet})`,
-  tablet: `screen and (max-width: ${deviceSizes.laptop})`,
+  laptop: `screen and (min-width: ${deviceSizes.laptop}px)`,
+  tablet: `screen and (max-width: ${deviceSizes.laptop - 0.1}px)`,
+  mobile: `screen and (max-width: ${deviceSizes.tablet - 0.1}px)`,
+  mobileS: `screen and (max-width: ${deviceSizes.mobile - 0.1}px)`,
+};
+
+const detailedMobile = {
+  tablet: `screen and (max-width: ${
+    deviceSizes.laptop - 0.1
+  }px) and (min-width: ${deviceSizes.tablet}px)`,
+  mobileL: `screen and (max-width: ${
+    deviceSizes.tablet - 0.1
+  }px) and (min-width: ${deviceSizes.mobile}px)`,
+  mobileS: `screen and (max-width: ${deviceSizes.mobile - 0.1}px)`,
+};
+
+const standardValues = {
+  card: {
+    maxWidth: '330px',
+    margin: '20px',
+    maxCount: {
+      top: 4,
+      tab: 3,
+      mobL: 2,
+      mobS: 1,
+    },
+  },
+  userList: {
+    maxCount: 10,
+    size: '5rem',
+  },
+  banner: {
+    maxCount: {
+      top: 5,
+      tab: 3,
+      mobL: 2,
+      mobS: 1,
+    },
+  },
 };
 
 const theme = {
   color,
   font,
   style,
-  device,
   common,
+  device,
+  detailedMobile,
+  standardValues,
 };
 
 export default theme;
