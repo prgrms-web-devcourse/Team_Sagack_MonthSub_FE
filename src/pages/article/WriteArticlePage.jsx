f4115e5278cbe431554e68b2053c26eff8d079f3
yoon <yunyun40550082@gmail.com>
2022-02-01 18:02:49 +0900

    refactor: [MON-302]ArticleForm의 api로직 Write/EditAritlclePage로 분리 후 props으로 넘겨주는 로직 구현

--- src/pages/article/WriteArticlePage.jsx
+++ src/pages/article/WriteArticlePage.jsx
@@ -2,14 +2,47 @@ import React from 'react';
 import { ArticleForm } from '@organisms';
 import { Wrapper } from '@templates';
 import styled from '@emotion/styled';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import { postArticle } from '@apis/article';
+import jsonBlob from '@utils/createJsonBlob';
 
 const WriteArticlePage = () => {
-  const { seriesId, articleId } = useParams();
+  const history = useHistory();
+  const { seriesId } = useParams();
+  const initialValues = {
+    thumbnailFile: '',
+    title: '',
+    contents: '',
+    thumbnailUrl: '',
+  };
+
+  const handleSubmit = async values => {
+    try {
+      const request = {
+        ...values,
+        seriesId,
+      };
+
+      const formData = new FormData();
+      formData.append('file', values.thumbnailFile);
+      formData.append('request', jsonBlob(request));
+
+      const response = await postArticle({
+        data: formData,
+      });
+
+      if (response.status === 200) {
+        history.push(`/series/${seriesId}`);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <Background>
       <Wrapper>
-        <ArticleForm param={{ seriesId, articleId }} />
+        <ArticleForm initialValues={initialValues} onSubmit={handleSubmit} />
       </Wrapper>
     </Background>
   );
