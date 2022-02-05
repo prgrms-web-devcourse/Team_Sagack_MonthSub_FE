8d56c7ce92cc7fe7f7c855ae93175fe7a5cef964
yoon <yunyun40550082@gmail.com>
2022-02-05 13:59:29 +0900

    feat: [MON-302] 모든 Icon컴포넌트 스토리북 등록

--- src/stories/components/atom/Icon.stories.jsx
+++ src/stories/components/atom/Icon.stories.jsx
@@ -6,20 +6,56 @@ export default {
   component: Icon,
 };
 
-export const Like = () => (
-  <Icon>
+export const Like = args => (
+  <Icon {...args}>
     <Icon.Like />
   </Icon>
 );
 
-export const Home = () => (
-  <Icon>
+export const LikeBorder = args => (
+  <Icon {...args}>
+    <Icon.LikeBorder />
+  </Icon>
+);
+
+export const Home = args => (
+  <Icon {...args}>
     <Icon.Home />
   </Icon>
 );
 
-export const User = () => (
-  <Icon>
+export const User = args => (
+  <Icon {...args}>
     <Icon.User />
+  </Icon>
+);
+
+export const Change = args => (
+  <Icon {...args}>
+    <Icon.Change />
+  </Icon>
+);
+
+export const Search = args => (
+  <Icon {...args}>
+    <Icon.Search />
+  </Icon>
+);
+
+export const Tag = args => (
+  <Icon {...args}>
+    <Icon.Tag />
+  </Icon>
+);
+
+export const AddCircle = args => (
+  <Icon {...args}>
+    <Icon.AddCircle />
+  </Icon>
+);
+
+export const Info = args => (
+  <Icon {...args}>
+    <Icon.Info />
   </Icon>
 );
