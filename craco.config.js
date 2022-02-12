b252cfcf457b91823fc3a7997bbd394432c36cc5
thddlmy <thddlmy@gmail.com>
2022-02-13 00:26:16 +0900

    feat: [MON-317] 타입스크립트 alias 적용 및 로딩 컴포넌트, theme 파일명 변경

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CracoAlias = require('craco-alias');
 
 module.exports = {
   babel: {
@@ -7,18 +8,18 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
-      // '@atom': path.resolve(__dirname, 'src/components/atom'),
-      // '@mocules': path.resolve(__dirname, 'src/components/mocules'),
-      // '@organisms': path.resolve(__dirname, 'src/components/organisms'),
-      // '@templates': path.resolve(__dirname, 'src/components/templates'),
-      // '@hooks': path.resolve(__dirname, 'src/hooks'),
-      // '@pages': path.resolve(__dirname, 'src/pages'),
-      // '@utils': path.resolve(__dirname, 'src/utils'),
-      // '@apis': path.resolve(__dirname, 'src/apis'),
+      '@atom': path.resolve(__dirname, 'src/components/atom'),
+      '@mocules': path.resolve(__dirname, 'src/components/mocules'),
+      '@organisms': path.resolve(__dirname, 'src/components/organisms'),
+      '@templates': path.resolve(__dirname, 'src/components/templates'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@apis': path.resolve(__dirname, 'src/apis'),
       // '@styles': path.resolve(__dirname, 'src/styles'),
-      // '@contexts': path.resolve(__dirname, 'src/contexts'),
-      // '@images': path.resolve(__dirname, 'src/images'),
-      // '@constants': path.resolve(__dirname, 'src/constants'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@images': path.resolve(__dirname, 'src/images'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
     },
     jest: {
       configure: {
@@ -28,4 +29,13 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
 };
