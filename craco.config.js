125fc575add9d961aacacd4e782c3b22faef8327
thddlmy <thddlmy@gmail.com>
2022-02-12 23:52:22 +0900

    feat: [MON-317] 타입스크립트 패키지 설치 및 세팅

--- craco.config.js
+++ craco.config.js
@@ -6,18 +6,26 @@ module.exports = {
   },
   webpack: {
     alias: {
-      '@atom': path.resolve(__dirname, 'src/components/atom'),
-      '@mocules': path.resolve(__dirname, 'src/components/mocules'),
-      '@organisms': path.resolve(__dirname, 'src/components/organisms'),
-      '@templates': path.resolve(__dirname, 'src/components/templates'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@apis': path.resolve(__dirname, 'src/apis'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@images': path.resolve(__dirname, 'src/images'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@': path.resolve(__dirname, 'src/'),
+      // '@atom': path.resolve(__dirname, 'src/components/atom'),
+      // '@mocules': path.resolve(__dirname, 'src/components/mocules'),
+      // '@organisms': path.resolve(__dirname, 'src/components/organisms'),
+      // '@templates': path.resolve(__dirname, 'src/components/templates'),
+      // '@hooks': path.resolve(__dirname, 'src/hooks'),
+      // '@pages': path.resolve(__dirname, 'src/pages'),
+      // '@utils': path.resolve(__dirname, 'src/utils'),
+      // '@apis': path.resolve(__dirname, 'src/apis'),
+      // '@styles': path.resolve(__dirname, 'src/styles'),
+      // '@contexts': path.resolve(__dirname, 'src/contexts'),
+      // '@images': path.resolve(__dirname, 'src/images'),
+      // '@constants': path.resolve(__dirname, 'src/constants'),
+    },
+    jest: {
+      configure: {
+        moduleNameMapper: {
+          '^@(.*)$': '<rootDir>/src$1',
+        },
+      },
     },
   },
 };
