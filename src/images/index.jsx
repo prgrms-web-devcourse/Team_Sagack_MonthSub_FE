af420ad7ff420c6cc60d0637bd58de0625ce5733
yoon <yunyun40550082@gmail.com>
2022-02-13 18:35:22 +0900

    refactor: [MON-318] image파일 import 방식 변경

---
+++ src/images/index.jsx
@@ -0,0 +1,3 @@
+export { default as whiteBackLogo } from './logo_whiteBackboard.svg';
+export { default as blackBackLogo } from './logo_blackBackboard.svg';
+export { default as channelCover } from './channel_cover.jpg';
