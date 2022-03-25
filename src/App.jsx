09c93bc4f431a834a424df3ed2de9745a335a73c
yoon <yunyun40550082@gmail.com>
2022-03-25 14:04:30 +0900

    chore: 불필요한 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -28,62 +28,51 @@ import {
 import PrivateRoute from './utils/privateRoute';
 import PublicRoute from './utils/publicRoute';
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <PublicRoute exact from="/signup" to="/" component={SignUpPage} />
-        <PublicRoute
-          exact
-          from="/signin"
-          to="/my/info"
-          component={SignInPage}
-        />
-        <PrivateRoute exact from="/my/info" component={MyInfoPage} />
-        <PrivateRoute exact from="/my/edit" component={EditMyInfoPage} />
-        <PrivateRoute exact from="/channel/my" component={ChannelPage} />
-        <Route path="/channel/:id" exact component={ChannelPage} />
-        <PrivateRoute
-          exact
-          from="/purchase/info"
-          component={PurchaseHistoryPage}
-        />
-        <Route path="/follow/my" exact component={FollowListPage} />
-        <Route path="/follow/:id" exact component={FollowListPage} />
-        <PrivateRoute exact from="/purchase/:id" component={PurchasePage} />
-        <PrivateRoute exact from="/my/likes" component={MyLikeSeriesPage} />
-        <Route path="/search" exact component={SearchPage} />
-        <PrivateRoute exact from="/writes" component={WriteListPage} />
-        <Route path="/series" exact component={SeriesListPage} />
-        <PrivateRoute exact from="/series/write" component={WriteSeriesPage} />
-        <PrivateRoute
-          exact
-          from="/series/edit/:id"
-          component={EditSeriesPage}
-        />
-        <Route path="/series/:id" exact component={SeriesDetailPage} />
-        <PrivateRoute
-          exact
-          from="/series/:seriesId/article/write"
-          component={WriteArticlePage}
-        />
-        <PrivateRoute
-          exact
-          from="/series/:seriesId/article/edit/:articleId"
-          component={EditArticlePage}
-        />
-        <PrivateRoute
-          exact
-          from="/series/:seriesId/article/:articleId"
-          component={ArticleDetailPage}
-        />
-        <Route path="/server-error" component={ServerErrorPage} />
-        <Route path="*" component={NotFoundPage} />
-      </Switch>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <PublicRoute exact from="/signup" to="/" component={SignUpPage} />
+      <PublicRoute exact from="/signin" to="/my/info" component={SignInPage} />
+      <PrivateRoute exact from="/my/info" component={MyInfoPage} />
+      <PrivateRoute exact from="/my/edit" component={EditMyInfoPage} />
+      <PrivateRoute exact from="/channel/my" component={ChannelPage} />
+      <Route path="/channel/:id" exact component={ChannelPage} />
+      <PrivateRoute
+        exact
+        from="/purchase/info"
+        component={PurchaseHistoryPage}
+      />
+      <Route path="/follow/my" exact component={FollowListPage} />
+      <Route path="/follow/:id" exact component={FollowListPage} />
+      <PrivateRoute exact from="/purchase/:id" component={PurchasePage} />
+      <PrivateRoute exact from="/my/likes" component={MyLikeSeriesPage} />
+      <Route path="/search" exact component={SearchPage} />
+      <PrivateRoute exact from="/writes" component={WriteListPage} />
+      <Route path="/series" exact component={SeriesListPage} />
+      <PrivateRoute exact from="/series/write" component={WriteSeriesPage} />
+      <PrivateRoute exact from="/series/edit/:id" component={EditSeriesPage} />
+      <Route path="/series/:id" exact component={SeriesDetailPage} />
+      <PrivateRoute
+        exact
+        from="/series/:seriesId/article/write"
+        component={WriteArticlePage}
+      />
+      <PrivateRoute
+        exact
+        from="/series/:seriesId/article/edit/:articleId"
+        component={EditArticlePage}
+      />
+      <PrivateRoute
+        exact
+        from="/series/:seriesId/article/:articleId"
+        component={ArticleDetailPage}
+      />
+      <Route path="/server-error" component={ServerErrorPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
+  </>
+);
 
 export default App;
